6961a3706fa132d5eea1371e66049e0f4a563d8a
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-11-13 20:43:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const message = "Hola, Bienvenido(a) a mi portafolio";
 
 alert(message);
 console.log(message);
+
+const head = document.head
+const body = document.body
+
+console.log(head, "\n", body);
