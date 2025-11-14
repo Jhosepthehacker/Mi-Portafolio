adb93ec7f5cdf43945c0bb63ff0121541260cdf1
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-11-13 20:27:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
+import sqlmodel as sql
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi import HTTPException
+from starlette.status import HTTP_403_FORBIDDEN
 
 app = FastAPI()
 app.title = "Mi Portafolio"
 
-@app.get('/', tags=["Home"])
+@app.get('/', tags=["Mi Portafolio"])
 def home():
     return "Hola, bienvenido(a) a mi portafolio"
+
+@app.post('/accounts', tags=["Registrarse"])
+def login(name: str, age: int, gmail: str, password: str):
+    data = []
+    
+    if (len(name) < 1) or (len(age) < 1) or (len(gmail) < 1) or (len(password) < 1):
+        return {"Warning": "Por favor complete los formularios"}
+    else:
+        pass
+
+    data.append(name, age, gmail, password)
